43ea771ccb2a90362c42f43231bf9ec15cc98056
marcellusc <96213317+marcellusc@users.noreply.github.com>
2022-11-10 02:45:32 -0800

    init

---
+++ wordpress 8/wp-content/plugins/cargo-tracking-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.4',
+        'version' => '1.0.4.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'ilhanaydinli/cargo_tracking_for_woocommerce',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'ilhanaydinli/cargo_tracking_for_woocommerce' => array(
+            'pretty_version' => '1.0.4',
+            'version' => '1.0.4.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
